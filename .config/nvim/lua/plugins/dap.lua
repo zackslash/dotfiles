f8318408c0ef8be814315ef89f867d0059a6a934
Luke Hines <luke@hines.im>
2026-03-18 21:08:54 +0000

    add Go debugger setup (nvim-dap + nvim-dap-go + nvim-dap-ui + delve)

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,69 @@
+return {
+  -- Core DAP client
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      -- DAP UI
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        keys = {
+          { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+          { "<leader>de", function() require("dapui").eval() end,   desc = "Eval expression", mode = { "n", "v" } },
+        },
+        opts = {},
+        config = function(_, opts)
+          local dap, dapui = require("dap"), require("dapui")
+          dapui.setup(opts)
+          -- Auto-open/close UI on debug events
+          dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+          dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+          dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+        end,
+      },
+      -- Virtual text showing variable values inline
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+    },
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end,                                desc = "Toggle breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Condition: ")) end,        desc = "Conditional breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end,                                         desc = "Continue" },
+      { "<leader>dn", function() require("dap").step_over() end,                                        desc = "Step over" },
+      { "<leader>di", function() require("dap").step_into() end,                                        desc = "Step into" },
+      { "<leader>do", function() require("dap").step_out() end,                                         desc = "Step out" },
+      { "<leader>dq", function() require("dap").terminate() end,                                        desc = "Terminate" },
+      { "<leader>dr", function() require("dap").repl.open() end,                                        desc = "Open REPL" },
+    },
+  },
+
+  -- Go adapter (uses delve)
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = { "mfussenegger/nvim-dap" },
+    opts = {
+      dap_configurations = {
+        {
+          type = "go",
+          name = "Debug current file",
+          request = "launch",
+          program = "${file}",
+        },
+        {
+          type = "go",
+          name = "Debug test",
+          request = "launch",
+          mode = "test",
+          program = "${file}",
+        },
+      },
+    },
+    keys = {
+      { "<leader>dt", function() require("dap-go").debug_test() end,         desc = "Debug nearest Go test" },
+      { "<leader>dT", function() require("dap-go").debug_last_test() end,    desc = "Re-run last Go test" },
+    },
+  },
+}
