c945665846235761b36004ae5c244f95128718ef
Luke Hines <luke@hines.im>
2026-03-18 21:45:30 +0000

    feat: add copilot.lua for inline ghost text completions

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = "<C-Right>",
+          accept_line = "<C-Down>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = false,
+      },
+    },
+  },
+}
