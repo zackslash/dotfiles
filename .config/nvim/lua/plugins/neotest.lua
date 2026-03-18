7c6dfb99047c9e02061fbcddd023dfd248a82a08
Luke Hines <luke@hines.im>
2026-03-18 21:54:00 +0000

    fix: call neotest-golang adapter as function

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -5,17 +5,19 @@ return {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "fredrikaverpil/neotest-golang",
+      { "fredrikaverpil/neotest-golang", version = "*" },
     },
-    opts = {
-      adapters = {
-        ["neotest-golang"] = {},
-      },
-    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(),
+        },
+      })
+    end,
     keys = {
       { "<leader>tt", function() require("neotest").run.run() end,                          desc = "Run nearest test" },
       { "<leader>tT", function() require("neotest").run.run(vim.fn.expand("%")) end,        desc = "Run test file" },
-      { "<leader>ts", function() require("neotest").run.run(vim.loop.cwd()) end,            desc = "Run test suite" },
+      { "<leader>ts", function() require("neotest").run.run(vim.uv.cwd()) end,              desc = "Run test suite" },
       { "<leader>tl", function() require("neotest").run.run_last() end,                     desc = "Re-run last test" },
       { "<leader>to", function() require("neotest").output.open({ enter = true }) end,      desc = "Show test output" },
       { "<leader>tO", function() require("neotest").output_panel.toggle() end,              desc = "Toggle output panel" },
